3547a52f8e9455d716e4198f5529ba3e2e7dc988
Luz Cazares <luz.cazares@intel.com>
2018-01-23 16:51:28 -0800

    Revert "Fix requirement installation"
    This reverts commit 98310b2cadbe97e8204d3078057f859cbcf72285.

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     test_suite='demo.tests',
     keywords = ['dlux', 'say_hi', 'hello_world', 'mypackage'],
 
-    install_requires = ['coverage>=3.6'],
     # Additional metadata
     classifier = [
         'Development Status :: 4 - Beta',
